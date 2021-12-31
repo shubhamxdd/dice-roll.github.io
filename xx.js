5bf5496fc284aaf0b5620c90c768cd01ec2b8688
shubhamxdd <ssisodia128@gmail.com>
2021-12-31 10:53:43 +0530

    added existing files

---
+++ xx.js
@@ -0,0 +1,21 @@
+var dice = {
+    sides: 6,
+    roll: function () {
+      var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+      return randomNumber;
+    }
+  }
+  function printNumber(number) {
+    var placeholder = document.getElementById('placeholder');
+    placeholder.innerHTML = number;
+  }
+  var button = document.getElementById('button');
+  
+  button.onclick = function() {
+    var result = dice.roll();
+    printNumber(result);
+  };
+  function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
